7274c97a1c3065da7693f7ad6ecd5f63eb11c830
rauchips <chipindebenjamin@gmail.com>
2021-09-08 23:44:24 +0300

    added async and falsified watchAll for jest

--- app.test.js
+++ app.test.js
@@ -3,33 +3,29 @@ const request = require('supertest');
 const app = require('./app')
 
 describe('Todos API', () => {
-  it('GET /  list of all to do activities', () => {
-    return request(app)
+  it('GET /  list of all to do activities', async () => {
+    const response = await request(app)
       .get('/')
       .expect('Content-Type', /json/)
-      .expect(200)
-      .then((response) => {
-        expect(response.body).toEqual(expect.arrayContaining([
-          expect.objectContaining({
-            id: expect.any(Number),
-            name: expect.any(String),
-            completed: expect.any(Boolean)
-          })
-        ]))
+      .expect(200);
+    expect(response.body).toEqual(expect.arrayContaining([
+      expect.objectContaining({
+        id: expect.any(Number),
+        name: expect.any(String),
+        completed: expect.any(Boolean)
       })
+    ]));
   })
-  it('GET /:id  list a specific to do activity', () => {
-    return request(app)
+  it('GET /:id  list a specific to do activity', async () => {
+    const response = await request(app)
       .get('/1')
       .expect('Content-Type', /json/)
-      .expect(200)
-      .then((response) => {
-        expect(response.body).toEqual(
-          expect.objectContaining({
-            id: expect.any(Number),
-            name: expect.any(String),
-            completed: expect.any(Boolean)
-          }))
-      })
+      .expect(200);
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        id: expect.any(Number),
+        name: expect.any(String),
+        completed: expect.any(Boolean)
+      }));
   })
 })
